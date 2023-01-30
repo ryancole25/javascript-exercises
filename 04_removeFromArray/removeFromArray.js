ea840ef1f7c631847c65bb649c61bf37bcf99ba2
ryancole25 <cole.ryan.n@gmail.com>
2023-01-29 19:40:33 -0500

    Solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...args) {
+    // Make an array with all of the values to remove
+    let removeThese = [];
+    for (let i = 1; i < arguments.length; i ++){
+        removeThese.push(arguments[i]);
+    }
 
+    let newArray = [];
+    let remove = false;
+    // Loop through the givenArray and see if the values should be removed or not
+    for (let j = 0; j < givenArray.length; j ++){
+        remove = false;
+        for (let k = 0; k < removeThese.length; k ++){
+            if (removeThese[k] === givenArray[j]){
+                remove = true;
+            }
+        }
+        // Add values to new array if they are not removed
+        if (remove == false){
+            newArray.push(givenArray[j]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line

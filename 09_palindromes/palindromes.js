96dd6b6a45207efcb8964bfd16957a621043affc
ryancole25 <cole.ryan.n@gmail.com>
2023-02-02 12:41:23 -0500

    Solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    const alphanumeric = /^[\p{L}\p{N}]*$/u;
+    let forward = '';
+    for (let i = 0; i < words.length; i ++){
+        if (words[i].match(alphanumeric)){
+            forward += words[i].toLowerCase();
+        }
+    }
 
+    let reverse = '';
+    for (let j = forward.length - 1; j >= 0; j += -1){
+        reverse += forward[j];
+    }
+
+    if (reverse == forward){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+

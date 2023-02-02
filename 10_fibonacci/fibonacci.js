85d552745349a128dc8b7314202aaf6167c5c07c
ryancole25 <cole.ryan.n@gmail.com>
2023-02-02 13:02:38 -0500

    Solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(fibNumber) {
+    if (fibNumber <= 0){
+        return "OOPS";
+    }
+    
+    fibArray = [1, 1];
+    if (fibNumber <= 2){
+        return fibArray[fibNumber - 1];
+    }
 
+    for (let i = 2; i < fibNumber; i += 1){
+        fibArray[i] = fibArray[i - 1] + fibArray[i - 2]
+    }
+    return fibArray[fibNumber - 1];
 };
 
 // Do not edit below this line

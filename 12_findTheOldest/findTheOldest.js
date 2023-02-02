b4e55c679245866c718e0cc74465620c5c3c95d4
ryancole25 <cole.ryan.n@gmail.com>
2023-02-02 13:20:56 -0500

    Solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(a, b){
+        if (!a.yearOfDeath){
+            a.yearOfDeath = 2023;
+        }
+        if (!b.yearOfDeath){
+            b.yearOfDeath = 2023;
+        }
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        if (aAge - bAge > 0){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    });
+    return oldest[0];
 
 };
 
